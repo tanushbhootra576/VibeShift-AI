0685f48468fa261b95a4b178cf9db92ab95c5cd4
tanushbhootra576 <tanushbhootratanush@gmail.com>
2026-06-29 09:54:39 +0530

    chore: remove one-off scaffolding and fix scripts from scripts/

--- scripts/fix-motiondiv.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/app/dashboard/page.tsx', 'utf-8');
-code = code.replace(/<MotionDiv/g, '<div');
-code = code.replace(/<\/MotionDiv>/g, '</div>');
-fs.writeFileSync('src/app/dashboard/page.tsx', code);
